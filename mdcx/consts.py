95846e5b102b4e97a2968393ecb6f78152e3d3ad
Hazard804 <82299110+Hazard804@users.noreply.github.com>
2026-02-09 20:09:37 +0800

    chore: 更新 localversion 到 220260212

--- mdcx/consts.py
+++ mdcx/consts.py
@@ -2,7 +2,7 @@ import platform
 import sys
 from pathlib import Path
 
-LOCAL_VERSION = 220260211
+LOCAL_VERSION = 220260212
 
 GITHUB_REPO = "Hazard804/mdcx"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
