1837f289cf3bc261113cf22f2503af50cf1a7056
z291173301 <zhou291173301@gmail.com>
2026-05-17 12:00:47 +0800

    Rename 'four_k' key to '4K' in FIELD_DESCRIPTIONS

--- mdcx/core/naming/fields.py
+++ mdcx/core/naming/fields.py
@@ -31,7 +31,7 @@ FIELD_DESCRIPTIONS: dict[str, str] = {
     "filename": "原文件名",
     "wanted": "想看人数",
     "score": "评分",
-    "four_k": "4K 标识",
+    "4K": "4K 标识",
 }
 
 FIELD_NAMES = tuple(FIELD_DESCRIPTIONS)
@@ -141,7 +141,7 @@ def build_naming_context(
         "filename": filename,
         "wanted": str(data.wanted or ""),
         "score": score,
-        "four_k": four_k,
+        "4K": four_k,
     }
     values = {field: _clean_field_value(value, escape_path_separator) for field, value in raw_values.items()}
     return NamingContext(values=values, raw_values=raw_values)
